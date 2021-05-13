528cfdd3f4ca399ac2cd8fdb4e5667cff02767bd
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-05-13 01:48:19 -0400

    Update LQuickSort.java

--- LQuickSort.java
+++ LQuickSort.java
@@ -1,20 +1,24 @@
 import com.java.base.*;
 
+Package L1ProjectionOutput.java;
+Package L2ProjectionOutput.java;
+Package L3ProjectionOutput.java;
+Package L4ProjectionOutput.java;
+Package L5ProjectionOutput.java;
+
 public static Arr[N];
 public static int low;
 public static int high;
 public static LArr[L1ProjectionOutput,L2ProjectionOutput,L3ProjectionOutput,L4ProjectionOutput,L5ProjectionOutput];
-public static final low = L1ProjectionOutput; 
-public static final high = L5ProjectionOutput; 
-public static N = LArr[n];
+public static final low = LArr[0]; 
+public static final high = LArr[4]; 
+public static N = LArr[];
  
 public class quicksort(){
 new quicksort = Quicksort();
-Quicksort(){
-for(n>>low<=LArr[5])==
-true;
-return(LArr[N]-1);
-for(n<<high>=LArr[1])==
-true;
-return(LArr[N]+1)};
-return Quicksort();
+Quicksort(LArr[n],n++1,n--1,n<=5){
+For(n==low);
+then(n++1);
+for(n==high);
+then(n--1)};
+return Quicksort(LArr[n]);
